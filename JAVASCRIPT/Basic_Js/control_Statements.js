cbcb0430e28430e8d0cfb994698fcf9f10340657
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-10 14:26:13 +0500

    Grades

---
+++ JAVASCRIPT/Basic_Js/control_Statements.js
@@ -0,0 +1,19 @@
+let marks=80;
+if(marks>=90){
+    console.log('A');
+}
+else if(marks>=80){
+    console.log('B');
+}
+else if(marks>=70){
+    console.log('C');
+}
+else if(marks>=60){
+    console.log('C');
+}
+else if(marks<60){
+    console.log('D');
+}
+else if(marks>100 && marks<1){
+    console.log('invalid');
+}
