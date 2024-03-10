30d58f43fae1d1d008a3bf2c5ba32d3ff7a47ff8
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-10 14:08:59 +0500

    Portfolio

---
+++ HTML_AND_CSS/Portfolio_Pro/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Veit:["Be Vietnam Pro","sans-serif"],
+      }, 
+    },
+  },
+  plugins: [],
+}
+
