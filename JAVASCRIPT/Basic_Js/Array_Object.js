8e7b0fd1aacaeb1e051c330da14c63430cdfc068
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-11 23:52:29 +0500

    arrow function

--- JAVASCRIPT/Basic_Js/Array_Object.js
+++ JAVASCRIPT/Basic_Js/Array_Object.js
@@ -2,7 +2,8 @@ let arr=[
     {no:1,namef:'farooq'},
     {no:2,namef:'virus'}
 ]
-let as=arr.find(function(bs){
-    return bs.namef==='virus';
-});
+// let as=arr.find(function(bs){
+//     return bs.namef==='virus';
+// });
+let as=arr.find(bs=>bs.namef==='virus');
 console.log(as);
