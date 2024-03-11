55d43c796b23a9f51490a0254150e7bbb41dc73d
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-11 21:07:47 +0500

    calculator

---
+++ JAVASCRIPT/Basic_Js/Calculator.js
@@ -0,0 +1,8 @@
+let num1=5;
+let num2=3;
+console.log("Sum=",(num1+num2));
+console.log("Sub=",(num1-num2));
+console.log("Pro=",(num1*num2));
+console.log("Div=",(num1/num2));
+console.log("Rem=",(num1%num2));
+console.log("Pow=",(num1**num2));
