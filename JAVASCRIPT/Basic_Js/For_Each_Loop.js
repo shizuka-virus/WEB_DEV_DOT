1d40f1afd6cc0514a5a8437f7deaa8e4864a76fb
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-12 21:43:02 +0500

    Arrow Function

--- JAVASCRIPT/Basic_Js/For_Each_Loop.js
+++ JAVASCRIPT/Basic_Js/For_Each_Loop.js
@@ -1,4 +1,5 @@
 let arr=[3,4,5,62,56,0];
-arr.forEach(function(num){
-    console.log(num);
-});
+// arr.forEach(function(num){
+//     console.log(num);
+// });
+arr.forEach(number=>console.log(number));
