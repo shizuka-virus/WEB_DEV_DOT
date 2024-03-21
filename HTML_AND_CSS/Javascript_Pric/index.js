a360ad807486df494e4afa7bcc96eb37604e45ff
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-21 19:21:00 +0500

    Adding string and number

--- HTML_AND_CSS/Javascript_Pric/index.js
+++ HTML_AND_CSS/Javascript_Pric/index.js
@@ -1,6 +1,9 @@
 // var thankx="Hello I am virus";
 // alert(thankx);
-var num=10;
-var num2=20;
-var sum=num+num2;
-alert("the sum=" + sum);
+// var num=10;
+// var num2=20;
+// var sum=num+num2;
+// alert("the sum=" + sum);
+// var num1=2;
+// var num2="4";
+// alert(num1+ +num2);
